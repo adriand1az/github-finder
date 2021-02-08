998c953610382c1ef693355a65a117cb66af636c
adriand1az <adrian111699@gmail.com>
2021-02-08 17:54:54 -0600

    'navbar'

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import React ,{
   Component
 } from 'react';
 
+import Navbar from './components/Navbar'
 // style
 import './App.css';
 
 
 class App extends Component {
   render(){
-      const name = 'John Doe';
-      const creator = 'Adrian Diaz'
       return (
     <div className="App">
-      <h1>Hello {name} From React!</h1>
-      <span>{creator} made this application</span>
+        <Navbar />
     </div>
   );
   }
