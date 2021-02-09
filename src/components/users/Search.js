6df0ef46d2a6b79639f12500ed007856e288c529
adriand1az <adrian111699@gmail.com>
2021-02-09 14:02:48 -0600

    search bar functionality

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+    state = {
+        text: ''
+    }
+
+    onChange = (e) => {
+        this.setState({text: e.target.value})
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.text)
+    }
     render() {
         return (
             <div>
-                <form className='form'>
-                    <input type='text' name='text' placeholder='Search Users'/>
+                <form className='form' onSubmit={this.onSubmit}>
+                    <input 
+                    type='text' 
+                    name='text' 
+                    placeholder='Search Users' 
+                    value={this.state.text}
+                    onChange={this.onChange}
+                    />
                     <input 
                     type='submit' 
                     value='Search'
